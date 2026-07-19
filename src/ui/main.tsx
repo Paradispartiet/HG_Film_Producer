22a64a6540ab6646e54a349ad59b39f87c9c8143
Paradispartiet <paradispartiet@gmail.com>
2026-07-19 22:05:26 +0200

    Load Production Cases density overrides

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 import "./styles.css";
+import "./productionCaseDensity.css";
 
 console.info("HG Film Producer booting");
 
